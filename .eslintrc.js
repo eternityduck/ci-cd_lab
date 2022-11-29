8337e5553a1ce5a2815e81540c8177a469c09eca
eternityduck <bogdan2003bk96@gmail.com>
2022-11-29 22:00:52 +0200

    Updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true
   },
-  extends: 'standard-with-typescript',
+  extends: ['standard-with-typescript', 'eslint:recommended',],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  rules: {
-  }
+  rules: {}
 }
